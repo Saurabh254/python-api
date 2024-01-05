17f7a2cf54d0d25ae3f7d1bc1cf5357c054ec75e
Saurabh Vishwakarma <75117608+Saurabh254@users.noreply.github.com>
2024-01-05 16:17:43 +0530

    Update database.py
    moved from static db url to environment url

--- app/internal/database.py
+++ app/internal/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from .schema import Base, Beer, BeerData
+import os 
 
-DATABASE_URI = 'postgresql+psycopg2://postgres:12345678@localhost/api'
+
+try:
+    DATABASE_URL = os.environ['DATABASE_URL']
+except Exception as e:
+    print("Add db environment variable")
+    exit() 
+# DATABASE_URI = 'postgresql+psycopg2://postgres:12345678@localhost/api'
 
 
 engine = create_engine(DATABASE_URI)
